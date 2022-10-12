db03324333e0f57b34011ca5bc0869b1aef0d21b
Iktiup-Mahmud <seyam.official96@gmail.com>
2022-10-12 17:46:37 +0600

    'eye-btn-added'

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -11,7 +11,7 @@ const NavBar = () => {
         <div>
             <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
                 <Container>
-                    <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
+                    <Navbar.Brand href="#home" className='text-secondary'><h2>Programming-Quiz</h2></Navbar.Brand>
                     <Navbar.Toggle aria-controls="responsive-navbar-nav" />
                     <Navbar.Collapse id="responsive-navbar-nav">
 
--- src/components/QuizQuestion/QuizQuestion.js
+++ src/components/QuizQuestion/QuizQuestion.js
@@ -2,23 +2,40 @@ import React from 'react';
 import Options from '../Options/Options';
 import { EyeIcon, EyeSlashIcon } from '@heroicons/react/24/solid'
 import './QuizQuestion.css'
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 const QuizQuestion = ({quiz}) => {
-    const {id, question, correctAnswer, options} = quiz;
-
+    const {question, correctAnswer, options} = quiz;
+    const handelEye = () => {
+        toast(`🦄 The correct answer is "${correctAnswer}"`, {
+            position: "top-center",
+            autoClose: 5000,
+            hideProgressBar: false,
+            closeOnClick: true,
+            pauseOnHover: true,
+            draggable: true,
+            progress: undefined,
+            theme: "dark",
+        });
+    }
+            
+            // });
+    
     
     return (
         <div className='border border-danger border-3 rounded-3 container m-5 p-4'>
             <div className='row'>
                 <h2 className='col-10'>Question: {question.slice(3, -4)}</h2>
-                <EyeIcon className='col-2 text-dark iconEye'></EyeIcon>
-                <EyeSlashIcon className='col-2 text-dark iconEye'></EyeSlashIcon>
+                <EyeIcon onClick={ () => handelEye()} className='col-2 text-dark iconEye'></EyeIcon>
             </div>
             <div className='row gx-5 gy-2 p-4'>
                 {
                     options.map(option => <Options option={option} correctAnswer={correctAnswer}></Options>)
                 }
             </div>
+
+            <ToastContainer/>
         </div>
     );
 };
